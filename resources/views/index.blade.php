1cdadad6454786f275ea6576e7728a2cd4712448
DarkcoderSe <kashifs294@gmail.com>
2022-05-23 16:03:32 +0500

    auth check added in masters

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,16 @@
     <div class="row justify-content-center mt-4 pt-4">
         <div class="col-md-5 text-center pt-4 mt-4">
 
+            @if (auth()->check())
+
             @if (auth()->user()->hasRole('agent'))
             <h3>Lead Generation System</h3>
             @elseif (auth()->user()->hasRole('bdm'))
             <h3>Sales Engine</h3>
             @else
             <h3>Management System</h3>
+            @endif
+
             @endif
 
         </div>
